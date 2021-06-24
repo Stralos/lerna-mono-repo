5a766020d58f72ad9060988cbfbd121950cb5573
kiselis.paulius <paulius.kiselis@railsbank.com>
2021-06-24 13:57:33 +0300

    remove junk files

--- packages/greeter/dist/src/greeter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.greeter = void 0;
-function greeter(user) {
-    var name = user.name, lastName = user.lastName;
-    return "Hello, " + name + " " + lastName + ", how are you ?";
-}
-exports.greeter = greeter;
