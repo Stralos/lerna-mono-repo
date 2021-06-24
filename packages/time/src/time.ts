de8dbb52bb92f718c458134e6206e1acde4d156d
kiselis.paulius <paulius.kiselis@railsbank.com>
2021-06-24 13:45:19 +0300

    Update text

--- packages/time/src/time.ts
+++ packages/time/src/time.ts
@@ -7,5 +7,5 @@ export function getDate(date: Date) {
 }
 
 export function greetWithTime(date: Date, user: User) {
-  return `${greeter(user)}. The current time is ${getDate(date)}`;
+  return `${greeter(user)}. Current time is ${getDate(date)}`;
 }
